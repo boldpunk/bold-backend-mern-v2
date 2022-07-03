259995db416cbc5395f7676b42dc846d17f8454d
Saidakbar <saidakbar9807@gmail.com>
2022-07-03 06:11:22 +0300

    new

--- index.js
+++ index.js
@@ -68,10 +68,3 @@ app.listen(process.env.PORT || 4444, (err) => {
 
   console.log('Server OK');
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-}
